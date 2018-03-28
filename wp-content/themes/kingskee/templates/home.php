4fbfe5e8ead004a0b2851cfb6a458469d178bb71
rkartzman <remykartzman@gmail.com>
2018-03-28 18:56:14 -0400

    Add styling for select. Add initial stripe setup

--- wp-content/themes/kingskee/templates/home.php
+++ wp-content/themes/kingskee/templates/home.php
@@ -75,6 +75,25 @@ $fields = get_fields();
 
 </div><!-- .how-it-works -->
 
+<?php /*
+    <script src="https://js.stripe.com/v3/"></script>
+
+    <form action="/charge" method="post" id="payment-form">
+      <div class="form-row">
+        <label for="card-element">
+          Credit or debit card
+        </label>
+        <div id="card-element">
+          <!-- A Stripe Element will be inserted here. -->
+        </div>
+
+        <!-- Used to display form errors. -->
+        <div id="card-errors" role="alert"></div>
+      </div>
+
+      <button>Submit Payment</button>
+    </form>
+     */  ?>
 <div class="main__form hero__block hero__block--relative">
     <div class="contain">
         <div class="inner">
